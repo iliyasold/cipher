51fcb30b511e247b440eeab6ea06a7ae1fcf69a4
iliyasold <iliyasold@gmail.com>
2018-03-19 00:58:09 +0300

    Added the encryption method SHA2

--- main.rb
+++ main.rb
@@ -1,23 +1,26 @@
 require 'digest'
 
-# Программа 'Шифрование фразы'. Soldatkin™Lab 2018. v.2.0
-
+# Программа 'Шифрование фразы'. Soldatkin™Lab 2018. v.2.1
+#
 puts "Введите слово или фразу для шифрования:"
 frase = STDIN.gets.chomp
 
 puts "Каким способом зашифровать:
 1. MD5
-2. SHA1"
+2. SHA1
+3. SHA2"
 
 method = STDIN.gets.to_i
 
-until method.between?(1, 2)
-  puts "Введите 1 или 2"
+until method.between?(1, 3)
+  puts "Введите 1, 2 или 3"
   method = STDIN.gets.to_i
 end
 
 if method == 1
   puts "Результат шифрования: #{Digest::MD5.hexdigest frase}"
-else
+elsif method == 2
   puts "Результат шифрования: #{Digest::SHA1.hexdigest frase}"
+else
+  puts "Результат шифрования: #{Digest::SHA2.hexdigest frase}"
 end
